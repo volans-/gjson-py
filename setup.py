ad3910098fe79e88940635c79d33a7d322dfc51a
Riccardo Coccioli <volans-@users.noreply.github.com>
2023-01-13 23:06:29 +0100

    setup.py, tox.ini: add Python 3.11 support
    * Mark Python 3.11 as supported.
    * Add Python 3.11 in the test matrix of tox.

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
